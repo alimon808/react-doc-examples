559f46b3ff79b4a96980dbb42f63737b97be9f63
adrianlimon <me@adrianlimon.com>
2018-03-20 13:42:32 -0700

    initial tic tac toe game

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import TicTacToe from './components/TicTacToe';
+
+ReactDOM.render(
+    <TicTacToe />,
+    document.getElementById('root')
+);
