213effd80c519bba8982bb939818aa7034e0a8a3
adrianlimon <me@adrianlimon.com>
2018-03-21 15:10:17 -0700

    create tic-tac-toe component directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TicTacToe from './components/TicTacToe';
+import TicTacToe from './components/tic-tac-toe/TicTacToe';
 
 ReactDOM.render(
     <TicTacToe />,
