4c3ca567662a7a57477692ef791f02ce055fe54f
adrianlimon <me@adrianlimon.com>
2018-04-23 11:15:08 -0700

    extracting component with keys

--- src/components/list-and-keys/simple-list.js
+++ src/components/list-and-keys/simple-list.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import ListItem from './list-item';
 
 export default function NumberList(props) {
     const numbers = props.numbers;
-    const listItems = numbers.map((number) => <li key={number.toString()}>{number * 2}</li>);
+    const listItems = numbers.map((number) => <ListItem key={number.toString()} value={number * 2} />);
     return (
         <ul>
             {listItems}
