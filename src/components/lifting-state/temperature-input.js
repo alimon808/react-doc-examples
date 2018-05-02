c77b675295676b700960f4689fd68894750751ed
adrianlimon <me@adrianlimon.com>
2018-05-02 12:40:32 -0700

    add TemperatureInput prop types

--- src/components/lifting-state/temperature-input.js
+++ src/components/lifting-state/temperature-input.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const scaleNames = {
     c: 'Celsius',
@@ -14,14 +15,20 @@ export default class TemperatureInput extends React.Component {
     handleChange(event) {
         this.props.onTemperatureChange(event.target.value);
     }
+
     render() {
         const scale = this.props.scale;
-        const temperature = this.props.temperature;
         return (
             <fieldset>
                 <legend>Enter temperature in {scaleNames[scale]}</legend>
-                <input type="number" value={parseFloat(temperature)} onChange={this.handleChange} />
+                <input type="text" value={parseFloat(this.props.temperature) || ''} onChange={this.handleChange} />
             </fieldset>
         );
     }
 }
+
+TemperatureInput.propTypes = {
+    temperature: PropTypes.string.isRequired,
+    scale: PropTypes.string.isRequired,
+    onTemperatureChange: PropTypes.func.isRequired
+};
